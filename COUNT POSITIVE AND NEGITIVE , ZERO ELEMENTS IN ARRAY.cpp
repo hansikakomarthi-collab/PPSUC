c5a351be1741ff28fad5ba210f72c614902d5191
Hansikakomarthi <hansikakomarthi@gmail.com>
2025-12-04 23:50:59 +0530

    Add files via upload

---
+++ COUNT POSITIVE AND NEGITIVE , ZERO ELEMENTS IN ARRAY.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos = 0, neg = 0, zero = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        if(a[i] > 0) pos++;
+        else if(a[i] < 0) neg++;
+        else zero++;
+    }
+
+    printf("Positive = %d\nNegative = %d\nZero = %d\n", pos, neg, zero);
+    return 0;
+}
+
